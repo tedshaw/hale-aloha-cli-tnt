2a444885d99470292f3ab11c8159217c370156b3
ToyLim@gmail.com <ToyLim@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-11-15 21:26:38 +0000

    [Issue 3] defined the command interface... still need to add throwable exceptions for the command parser.

---
+++ trunk/src/edu/hawaii/halealohacli/command/Command.java
@@ -0,0 +1,39 @@
+package edu.hawaii.halealohacli.command;
+
+/**
+ * Define the Command interface.
+ * 
+ * @author Toy Lim
+ */
+public interface Command {
+  /**
+   * Return the command String.
+   * 
+   * @return command String
+   */
+  public String toString();
+  
+  /**
+   * Return the command syntax String.
+   * 
+   * @return syntax String
+   */
+  public String getSyntax();
+
+  /**
+   * Return the command description String.
+   * 
+   * @return description String
+   */
+  public String getDescription();
+
+  /**
+   * Execute the command given the list of arguments.
+   * 
+   * TODO: should add and throw parser exceptions
+   * 
+   * @param args List of String arguments
+   * @return true if command was executed, false if error
+   */
+  public Boolean execute(String... args);
+}
