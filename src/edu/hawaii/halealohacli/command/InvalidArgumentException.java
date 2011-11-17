ab87c74901133661f02a2a8da5a3138e7fde1a02
ToyLim@gmail.com <ToyLim@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-11-17 04:07:32 +0000

    [Issue 8] Added InvalidArgumentException and [Issue 3] set Command.execute() to throw InvalidArgumentException when needed.

---
+++ src/edu/hawaii/halealohacli/command/InvalidArgumentException.java
@@ -0,0 +1,38 @@
+package edu.hawaii.halealohacli.command;
+
+/**
+ * InvalidArgumentException: whenever an argument parser encounters an
+ * invalid argument, this exception should be thrown.
+ * 
+ * @author Toy Lim
+ */
+public class InvalidArgumentException extends Exception {
+  /**
+   * Default constructor.
+   */
+  public InvalidArgumentException() {
+    super("The given argument is invalid.");
+  }
+
+  /**
+   * Constructor with a custom message.
+   * 
+   * @param message Error message String
+   */
+  public InvalidArgumentException(String message) {
+    super("The given argument is invalid: " + message);
+  }
+
+  /**
+   * Determines if a de-serialized file is compatible with this class.
+   * 
+   * Maintainers must change this value if and only if the new version of this class is not
+   * compatible with old versions. See Sun docs for <a
+   * href=http://java.sun.com/products/jdk/1.1/docs/guide /serialization/spec/version.doc.html>
+   * details. </a>
+   * 
+   * Not necessary to include in first version of the class, but included here as a reminder of its
+   * importance.
+   */
+  private static final long serialVersionUID = 1790884005718859161L;
+}
