13cfcc92feb9c7dab0f736f182a904b9f83cebae
ToyLim@gmail.com <ToyLim@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-11-17 20:58:45 +0000

    [Issue 14] Added a static implementation of CommandManager.

---
+++ src/edu/hawaii/halealohacli/command/CommandManager.java
@@ -0,0 +1,33 @@
+package edu.hawaii.halealohacli.command;
+
+import java.util.List;
+import java.util.Arrays;
+import org.wattdepot.client.WattDepotClient;
+
+/**
+ * return a list of instances of all the Command classes.
+ * 
+ * @author Toy Lim
+ */
+public class CommandManager {
+  private List<Command> commandList;
+
+  /**
+   * Default constructor.
+   * 
+   * @param client WattDepotClient client for data query
+   */
+  public CommandManager(WattDepotClient client) {
+    commandList =
+        Arrays.asList(new CurrentPower(client), new DailyEnergy(client), new EnergySince(client));
+  }
+
+  /**
+   * Return a list of instances of all the Command classes.
+   * 
+   * @return an array of instances of all the Command classes
+   */
+  public Command[] getCommands() {
+    return commandList.toArray(new Command[commandList.size()]);
+  }
+}
