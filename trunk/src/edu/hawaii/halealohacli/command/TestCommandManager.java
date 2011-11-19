bc8caa3a50d055b6f468a2269bfb37ef9b7ee089
tedshaw@gmail.com <tedshaw@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-11-19 17:23:50 +0000

    [Issue 1] Fixed TestCommandManager

--- trunk/src/edu/hawaii/halealohacli/command/TestCommandManager.java
+++ trunk/src/edu/hawaii/halealohacli/command/TestCommandManager.java
@@ -1,6 +1,5 @@
 package edu.hawaii.halealohacli.command;
 
-//import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
