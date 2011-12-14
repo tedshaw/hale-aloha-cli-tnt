30d045a10e0199dd9a54d227c1a947799f6582c0
tnchida@gmail.com <tnchida@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-12-14 07:36:45 +0000

    [issue 39] added Timer and TimerTask functionality in MonitorGoal.

--- trunk/src/edu/hawaii/halealohacli/command/MonitorGoal.java
+++ trunk/src/edu/hawaii/halealohacli/command/MonitorGoal.java
@@ -1,8 +1,12 @@
 package edu.hawaii.halealohacli.command;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.xml.datatype.XMLGregorianCalendar;
 import org.wattdepot.client.BadXmlException;
 import org.wattdepot.client.WattDepotClient;
@@ -34,6 +38,8 @@ public class MonitorGoal implements Command {
   private String commandDescription;
 
   private WattDepotClient wattDepotClient;
+  private Timer timer = new Timer();
+  private InputStream input = System.in;
 
   /**
    * Default constructor.
@@ -44,9 +50,9 @@ public class MonitorGoal implements Command {
   public MonitorGoal(WattDepotClient client) {
     commandString = "monitor-goal";
     commandSyntax = commandString + " [tower | lounge] goal interval";
-    commandDescription =
-        "Prints out a timestamp, the current power being consumed by the [tower"
-            + " | lounge], and whether or not the lounge is meeting its power conservation goal.";
+    commandDescription = "Every [interval] seconds, prints out a timestamp, the current power " +
+        "being consumed by the [tower | lounge], and whether or not the lounge is meeting its" +
+        " power conservation goal.";
     wattDepotClient = client;
   }
 
@@ -91,65 +97,63 @@ public class MonitorGoal implements Command {
    */
   @Override
   public void execute(String... args) throws InvalidArgumentException {
+    final String[] arguments = args;
     StringBuilder sb = checkAll(args);
     // All the arguments are valid.
     if (sb == null || sb.toString().isEmpty()) {
-      String source = args[0];
-      int goal = Integer.parseInt(args[1]);
-      XMLGregorianCalendar currentTime = null, timeStamp = null;
-      currentTime = Tstamp.makeTimestamp();
-      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
-      System.out.format("The current time is: %s\n",
-          format.format(new Date(currentTime.toGregorianCalendar().getTimeInMillis())));
-      // System.out.println(currentTime);
-      try {
-        // Get and print out the current power consumption.
-        SensorData data = wattDepotClient.getLatestSensorData(source);
-        timeStamp = data.getTimestamp();
-        double currentPow = data.getPropertyAsDouble(SensorData.POWER_CONSUMED) / 1000.0;
-        // System.out.println(timeStamp.getHour());
-        final String CONGRATS = "Congratulations, you are meeting your power consumption goal!";
-        final String FAIL =
-            "I'm sorry, you are not currently meeting your power consumption "
-                + "goal. Please conserve a little more energy.";
-        double baseline = 0;
-        // get the baseline value for the current hour.
-        for (int i = 0; i < 24; i++) {
-          if (timeStamp.getHour() == i) {
-            baseline = SetBaseline.baselines[i] / 1000.0;
-            break;
+      int interval = Integer.parseInt(args[2]);
+
+      TimerTask task = new TimerTask() {
+        // internal run method for task
+        public void run() {
+          try {
+            processor(arguments);
+          }
+          // processor will take care of printing out specific error messages.
+          catch (InvalidArgumentException e) {
+            System.err.println("Please try again.");
           }
         }
-        System.out.format("The baseline for %s is %.1f kWh.\n", source, baseline);
-        double desiredConsumption = calculateGoal(goal, baseline);
-        System.out.format("To meet your goal of a %s percent reduction in power consumption, you"
-            + " must be using less than %.1f kW of power.\n", goal, desiredConsumption);
-        System.out.format("As of %s, %s was using %.1f kW of power.\n", source,
-            format.format(new Date(timeStamp.toGregorianCalendar().getTimeInMillis())), currentPow);
-        if (isGoalBeingMet(desiredConsumption, currentPow)) {
-          System.out.println(CONGRATS);
-        }
-        else {
-          System.out.println(FAIL);
+      };
+
+      timer.scheduleAtFixedRate(task, 0, interval * 1000);
+
+      try {
+        // task is running, enter loop to check for user input.
+        while (input.available() == 0) {
+          try {
+            Thread.sleep(100);
+          }
+          catch (InterruptedException e) {
+            // catch InterruptedException. don't do anything.
+          }
         }
       }
-      catch (BadXmlException e) {
-        XMLGregorianCalendar firstData = null;
-        try {
-          firstData = wattDepotClient.getSourceSummary(source).getFirstSensorData();
-        }
-        catch (WattDepotClientException e1) {
-          System.err.println("Error attempting to access data from " + source);
-          return;
-        }
-        throw new InvalidArgumentException("Error attempting to access data from date. Please "
-            + "use a date on or after "
-            + format.format(new Date(firstData.toGregorianCalendar().getTimeInMillis())),
-            (Throwable) e);
+      catch (IOException e) {
+        // catches IOException, issue with input stream.
+        System.out.println("Unfortunately, there was a problem with the input stream. "
+            + "Please choose another command.");
       }
-      catch (WattDepotClientException e) {
-        throw new InvalidArgumentException("Error attempting to access data from " + source,
-            (Throwable) e);
+
+      // loop finished, either user cancelled or issue with input stream, cancel task.
+      task.cancel();
+
+      try {
+        Thread.sleep(300);
+      }
+      catch (InterruptedException e) {
+        // catches InterrruptedException. don't do anything.
+      }
+
+      try {
+        // clear input buffer so that we don't get an invalid command error!
+        while (input.available() > 0) {
+          input.read();
+        }
+      }
+      catch (IOException e) {
+        // catches IOException. don't do anything.
+        System.out.print("");
       }
     }
     else {
@@ -157,6 +161,7 @@ public class MonitorGoal implements Command {
     }
   } // end execute()
 
+  
   /**
    * Method for validating the goal argument.
    * 
@@ -176,6 +181,7 @@ public class MonitorGoal implements Command {
     }
   } // end checkGoal().
 
+  
   /**
    * Method for validating the interval argument.
    * 
@@ -195,6 +201,7 @@ public class MonitorGoal implements Command {
     }
   } // end checkInterval().
 
+  
   /**
    * Checks all the command line arguments, as well as whether the baseline was set or not.
    * 
@@ -215,8 +222,8 @@ public class MonitorGoal implements Command {
     String regex = "^(Mokihana|Ilima|Lehua|Lokelani)[-]?[A-E]?$";
     StringBuilder sb = new StringBuilder();
     if (!source.matches(regex)) {
-      sb.append("Please enter a valid tower or lounge name. "
-          + "Type 'help' to see the available options.\n");
+      sb.append("Please enter a valid tower or lounge name. Type 'help' to see the available "
+          + "options.\n");
     }
     if (!checkGoal(goal)) {
       sb.append("You must enter an integer between 1 and 99 (inclusive) as a goal.\n");
@@ -255,4 +262,71 @@ public class MonitorGoal implements Command {
     }
     return false;
   } // end isGoalBeingMet()
+
+  
+  /**
+   * The main processor for the MonitorGoal class. Runs the checking methods and prints out all
+   * the information and data.
+   * @param args The command line arguments.
+   * @throws InvalidArgumentException If an invalid number of arguments were given.
+   */
+  public void processor(String... args) throws InvalidArgumentException {
+    String source = args[0];
+    int goal = Integer.parseInt(args[1]);
+    XMLGregorianCalendar currentTime = null, timeStamp = null;
+    currentTime = Tstamp.makeTimestamp();
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+    System.out.format("The current time is: %s\n",
+        format.format(new Date(currentTime.toGregorianCalendar().getTimeInMillis())));
+    // System.out.println(currentTime);
+    try {
+      // Get and print out the current power consumption.
+      SensorData data = wattDepotClient.getLatestSensorData(source);
+      timeStamp = data.getTimestamp();
+      double currentPow = data.getPropertyAsDouble(SensorData.POWER_CONSUMED) / 1000.0;
+      // System.out.println(timeStamp.getHour());
+      final String CONGRATS = "Congratulations, you are meeting your power consumption goal!";
+      final String FAIL =
+          "I'm sorry, you are not currently meeting your power consumption goal. "
+              + "Please conserve a little more energy.";
+      double baseline = 0;
+      // get the baseline value for the current hour.
+      for (int i = 0; i < 24; i++) {
+        if (timeStamp.getHour() == i) {
+          baseline = SetBaseline.baselines[i] / 1000.0;
+          break;
+        }
+      }
+      System.out.format("The baseline for %s is %.1f kWh.\n", source, baseline);
+      double desiredConsumption = calculateGoal(goal, baseline);
+      System.out.format("To meet your goal of a %s percent reduction in power consumption, you"
+          + " must be using less than %.1f kW of power.\n", goal, desiredConsumption);
+      System.out.format("As of %s, %s was using %.1f kW of power.\n", source,
+          format.format(new Date(timeStamp.toGregorianCalendar().getTimeInMillis())), currentPow);
+      if (isGoalBeingMet(desiredConsumption, currentPow)) {
+        System.out.println(CONGRATS);
+      }
+      else {
+        System.out.println(FAIL);
+      }
+    }
+    catch (BadXmlException e) {
+      XMLGregorianCalendar firstData = null;
+      try {
+        firstData = wattDepotClient.getSourceSummary(source).getFirstSensorData();
+      }
+      catch (WattDepotClientException e1) {
+        System.err.println("Error attempting to access data from " + source);
+        return;
+      }
+      throw new InvalidArgumentException("Error attempting to access data from date. Please "
+          + "use a date on or after " + format.format(new Date(firstData.toGregorianCalendar().
+              getTimeInMillis())), (Throwable) e);
+    }
+    catch (WattDepotClientException e) {
+      throw new InvalidArgumentException("Error attempting to access data from " + source,
+          (Throwable) e);
+    }
+  } // end processor()
+  
 } // end MonitorGoal.
