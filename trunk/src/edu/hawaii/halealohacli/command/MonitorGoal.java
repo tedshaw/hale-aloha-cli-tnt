94175ab4930379ec77388d6382d29c5ee37b35c3
tnchida@gmail.com <tnchida@gmail.com@a34b4029-8ca0-f214-5c33-9bbeb59ea0f1>
2011-12-14 05:35:23 +0000

    [issue 39] Added checkAll, calculateGoal, and isGoalBeingMet, along with appropriate outputs.

--- trunk/src/edu/hawaii/halealohacli/command/MonitorGoal.java
+++ trunk/src/edu/hawaii/halealohacli/command/MonitorGoal.java
@@ -11,7 +11,8 @@ import org.wattdepot.resource.sensordata.jaxb.SensorData;
 import org.wattdepot.util.tstamp.Tstamp;
 
 /**
- * Implement the monitor-goal Command interface. <br><br>
+ * Implement the monitor-goal Command interface. <br>
+ * <br>
  * 
  * Usage: monitor-goal [tower | lounge] goal interval <br>
  * Prints out a timestamp, the current power being consumed by the [tower | lounge], and whether or
@@ -27,8 +28,11 @@ import org.wattdepot.util.tstamp.Tstamp;
  */
 public class MonitorGoal implements Command {
   private String commandString;
+
   private String commandSyntax;
+
   private String commandDescription;
+
   private WattDepotClient wattDepotClient;
 
   /**
@@ -41,8 +45,8 @@ public class MonitorGoal implements Command {
     commandString = "monitor-goal";
     commandSyntax = commandString + " [tower | lounge] goal interval";
     commandDescription =
-        "Prints out a timestamp, the current power being consumed by the [tower | lounge], and"
-            + "whether or not the lounge is meeting its power conservation goal.";
+        "Prints out a timestamp, the current power being consumed by the [tower"
+            + " | lounge], and whether or not the lounge is meeting its power conservation goal.";
     wattDepotClient = client;
   }
 
@@ -87,32 +91,47 @@ public class MonitorGoal implements Command {
    */
   @Override
   public void execute(String... args) throws InvalidArgumentException {
-    if (args == null || args.length < 1) {
-      throw new InvalidArgumentException("No argument is given.");
-    }
-    if (args.length < 3) {
-      throw new InvalidArgumentException("Some argument is missing.");
-    }
-    String source = args[0];
-    String goal = args[1];
-    String interval = args[2];
-    if (!checkGoal(goal)) {
-      System.err.println("You must enter an integer between 1 and 99 (inclusive) as a goal.");
-    }
+    StringBuilder sb = checkAll(args);
     // All the arguments are valid.
-    if (checkInterval(interval)) {
+    if (sb == null || sb.toString().isEmpty()) {
+      String source = args[0];
+      int goal = Integer.parseInt(args[1]);
       XMLGregorianCalendar currentTime = null, timeStamp = null;
       currentTime = Tstamp.makeTimestamp();
-      System.out.println(currentTime);
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+      System.out.format("The current time is: %s\n",
+          format.format(new Date(currentTime.toGregorianCalendar().getTimeInMillis())));
+      // System.out.println(currentTime);
       try {
         // Get and print out the current power consumption.
         SensorData data = wattDepotClient.getLatestSensorData(source);
         timeStamp = data.getTimestamp();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
-        System.out.format("%s's power as of %s was %.1f kW.\n", source,
-            format.format(new Date(timeStamp.toGregorianCalendar().getTimeInMillis())),
-            data.getPropertyAsDouble(SensorData.POWER_CONSUMED) / 1000.0);
-        System.out.println(timeStamp.getHour());
+        double currentPow = data.getPropertyAsDouble(SensorData.POWER_CONSUMED) / 1000.0;
+        // System.out.println(timeStamp.getHour());
+        final String CONGRATS = "Congratulations, you are meeting your power consumption goal!";
+        final String FAIL =
+            "I'm sorry, you are not currently meeting your power consumption "
+                + "goal. Please conserve a little more energy.";
+        double baseline = 0;
+        // get the baseline value for the current hour.
+        for (int i = 0; i < 24; i++) {
+          if (timeStamp.getHour() == i) {
+            baseline = SetBaseline.baselines[i] / 1000.0;
+            break;
+          }
+        }
+        System.out.format("The baseline for %s is %.1f kWh.\n", source, baseline);
+        double desiredConsumption = calculateGoal(goal, baseline);
+        System.out.format("To meet your goal of a %s percent reduction in power consumption, you"
+            + " must be using less than %.1f kW of power.\n", goal, desiredConsumption);
+        System.out.format("As of %s, %s was using %.1f kW of power.\n", source,
+            format.format(new Date(timeStamp.toGregorianCalendar().getTimeInMillis())), currentPow);
+        if (isGoalBeingMet(desiredConsumption, currentPow)) {
+          System.out.println(CONGRATS);
+        }
+        else {
+          System.out.println(FAIL);
+        }
       }
       catch (BadXmlException e) {
         XMLGregorianCalendar firstData = null;
@@ -123,10 +142,9 @@ public class MonitorGoal implements Command {
           System.err.println("Error attempting to access data from " + source);
           return;
         }
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-        throw new InvalidArgumentException(
-            "Error attempting to access data from date. Please use a date on or after "
-                + format.format(new Date(firstData.toGregorianCalendar().getTimeInMillis())),
+        throw new InvalidArgumentException("Error attempting to access data from date. Please "
+            + "use a date on or after "
+            + format.format(new Date(firstData.toGregorianCalendar().getTimeInMillis())),
             (Throwable) e);
       }
       catch (WattDepotClientException e) {
@@ -134,10 +152,8 @@ public class MonitorGoal implements Command {
             (Throwable) e);
       }
     }
-
     else {
-      System.err.println("You must enter an integer greater than 0 as the interval.");
-
+      System.err.println(sb.toString());
     }
   } // end execute()
 
@@ -146,9 +162,8 @@ public class MonitorGoal implements Command {
    * 
    * @param g The String to validate.
    * @return True if the String is a valid goal, and false otherwise.
-   * @throws InvalidArgumentException If an invalid argument is given.
    */
-  public static boolean checkGoal(String g) throws InvalidArgumentException {
+  public static boolean checkGoal(String g) {
     try {
       int goal = Integer.parseInt(g);
       if (goal < 1 || goal > 99) {
@@ -166,9 +181,8 @@ public class MonitorGoal implements Command {
    * 
    * @param i The String to validate.
    * @return True if the String is a valid interval, and false otherwise.
-   * @throws InvalidArgumentException If an invalid argument is given.
    */
-  public static boolean checkInterval(String i) throws InvalidArgumentException {
+  public static boolean checkInterval(String i) {
     try {
       int interval = Integer.parseInt(i);
       if (interval < 1) {
@@ -180,4 +194,65 @@ public class MonitorGoal implements Command {
       return false;
     }
   } // end checkInterval().
+
+  /**
+   * Checks all the command line arguments, as well as whether the baseline was set or not.
+   * 
+   * @param args The command line arguments.
+   * @return A StringBuilder containing the errors raised, if any.
+   * @throws InvalidArgumentException If an invalid number of arguments were given.
+   */
+  public static StringBuilder checkAll(String[] args) throws InvalidArgumentException {
+    if (args == null || args.length < 1) {
+      throw new InvalidArgumentException("No argument is given.");
+    }
+    if (args.length < 3) {
+      throw new InvalidArgumentException("Some argument is missing.");
+    }
+    String source = args[0];
+    String goal = args[1];
+    String interval = args[2];
+    String regex = "^(Mokihana|Ilima|Lehua|Lokelani)[-]?[A-E]?$";
+    StringBuilder sb = new StringBuilder();
+    if (!source.matches(regex)) {
+      sb.append("Please enter a valid tower or lounge name. "
+          + "Type 'help' to see the available options.\n");
+    }
+    if (!checkGoal(goal)) {
+      sb.append("You must enter an integer between 1 and 99 (inclusive) as a goal.\n");
+    }
+    if (!checkInterval(interval)) {
+      sb.append("You must enter an integer greater than 0 as the interval.\n");
+    }
+    if (!SetBaseline.checkArray()) {
+      sb.append("You must set the baseline first. Enter 'help' to learn how to do this.\n");
+    }
+    return sb;
+  } // end checkAll()
+
+  /**
+   * Calculates the target power consumption goal.
+   * 
+   * @param goal The goal as a percentage in power consumption reduction.
+   * @param baseline The amount to base the reduction off of.
+   * @return The target power consumption amount as a double.
+   */
+  public static double calculateGoal(int goal, double baseline) {
+    double percent = 100 - goal;
+    return baseline * percent / 100.0;
+  } // end calculateGoal()
+
+  /**
+   * Checks whether the power consumption goal is being met for the given tower or lounge.
+   * 
+   * @param goal The targeted amount to be consuming.
+   * @param current The actual amount of power being currently consumed.
+   * @return True if the goal is being met, and false otherwise.
+   */
+  public static boolean isGoalBeingMet(double goal, double current) {
+    if (current <= goal) {
+      return true;
+    }
+    return false;
+  } // end isGoalBeingMet()
 } // end MonitorGoal.
